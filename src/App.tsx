59394811b816794fcbc12b795767727251ceb217
Laskellyb <anikwenzekelly@gmail.com>
2023-02-05 18:30:31 +0100

    uninstalled heroicons and tremor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import "@tremor/react/dist/esm/tremor.css";
+import Home from "./pages/Home";
 function App() {
-  return <div className="text-2xl bg-gray-100 font-bold">I am here</div>;
+  return (
+    <div className="text-2xl bg-gray-100 font-bold">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
