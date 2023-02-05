6bdf08967191eb57e485790c33ecebc78686c40a
Laskellyb <anikwenzekelly@gmail.com>
2023-02-05 17:56:35 +0100

    fixed the tailwind not working cos i set it as custom theme instead of extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "green-primary": "#097969",
-      "green-secondary": "#065550",
-      "aqua-color": "#c4fffb",
-      "maroon-color": "#77090e",
-      "gold-color": "#eba554",
+    extend: {
+      colors: {
+        "green-primary": "#097969",
+        "green-secondary": "#065550",
+        "aqua-color": "#c4fffb",
+        "maroon-color": "#77090e",
+        "gold-color": "#eba554",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
-    fontFamily: {
-      inter: ["Inter", "sans-serif"],
-      roboto: ["Roboto", "sans-serif"],
-    },
-    extend: {},
   },
   plugins: [],
 };
