a1d0185de15c2cb5ce235407c2589e453a38f811
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-07-18 19:25:34 +0100

    chore: change icons for the newsgrid

--- src/components/News/NewsGrid.tsx
+++ src/components/News/NewsGrid.tsx
@@ -1,9 +1,29 @@
 import { Grid, Card } from "@tremor/react";
-import { MdPublic } from "react-icons/md";
-import { useNewsContext } from "contexts";
+import {
+  MdPublic,
+  MdOutlineSocialDistance,
+  MdOutlineHealthAndSafety,
+  MdOutlineScience,
+  MdSportsMartialArts,
+} from "react-icons/md";
+import { BiSolidBusiness } from "react-icons/bi";
+import { GiTechnoHeart } from "react-icons/gi";
+import { useFilterContext, useNewsContext } from "contexts";
+
+const categoryIcons = {
+  all: MdPublic,
+  business: BiSolidBusiness,
+  entertainment: MdOutlineSocialDistance,
+  health: MdOutlineHealthAndSafety,
+  science: MdOutlineScience,
+  sports: MdSportsMartialArts,
+  technology: GiTechnoHeart,
+};
+
 export const NewsGrid = () => {
   const { newsApiArticles } = useNewsContext();
-  // todo - filter section for the news
+  const { activeNewsTab } = useFilterContext();
+
   return (
     <>
       <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2">
@@ -16,6 +36,8 @@ export const NewsGrid = () => {
           const date = new Date(publishedAt);
           const formattedDate = date.toDateString();
 
+          const IconComponent = categoryIcons[activeNewsTab];
+
           return (
             <Card key={index}>
               <section className="mb-6 lg:mb-0">
@@ -25,19 +47,22 @@ export const NewsGrid = () => {
                   data-te-ripple-color="light"
                 >
                   <img
-                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/018.jpg"
+                    src={`https://source.unsplash.com/random/?${activeNewsTab}`}
                     className="w-full"
-                    alt="Louvre"
+                    alt="news url"
+                    style={{ maxHeight: "200px" }}
                   />
-                  <a href="#!">
+                  <a href={url}>
                     <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"></div>
                   </a>
                 </div>
 
                 <div className="p-8 space-y-3  rounded-xl">
-                  <span className="inline-block text-green-700">
-                    <MdPublic className="w-8 h-8" />
-                  </span>
+                  {IconComponent && (
+                    <span className="inline-block text-green-700">
+                      <IconComponent className="w-8 h-8" />
+                    </span>
+                  )}
                   <p className="text-gray-500">
                     {formattedDate} by{" "}
                     <a
