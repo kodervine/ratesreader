ca205366466ef5b2155bbe2ad8bf063703fae39c
Chinenye Anikwenze <anikwenzekelly@gmail.com>
2023-08-02 19:00:19 +0100

    reproducing bug

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,8 +1,8 @@
-export * from "./NewsApiContext";
-export * from "./ForexApiContext";
-export * from "./filterTabsContext";
-export * from "./currencyConverterContext";
-export * from "./SelectedCurrencyContext";
-export * from "./utils";
-export * from "./ExchangeRateByDateContext";
-export * from "./FluctuationContext";
+// export * from "./NewsApiContext";
+// export * from "./ForexApiContext";
+// export * from "./filterTabsContext";
+// export * from "./currencyConverterContext";
+// export * from "./SelectedCurrencyContext";
+// export * from "./utils";
+// export * from "./ExchangeRateByDateContext";
+// export * from "./FluctuationContext";
